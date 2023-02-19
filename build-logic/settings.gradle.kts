84095914a4ca7152df316d367557a588b08aefff
Владислав <vladislavsumin@mail.ru>
2023-02-19 12:35:44 +0300

    setup repositories for build logic

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,4 +1,12 @@
 dependencyResolutionManagement {
+
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+    }
+
     versionCatalogs {
         create("libs") {
             from(files("../core-libs.versions.toml"))
