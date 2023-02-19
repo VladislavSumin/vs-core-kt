2dd6793ca0b06b635ed4b6b1697413dba0335327
Владислав <vladislavsumin@mail.ru>
2023-02-19 12:31:44 +0300

    add core-libs version.toml

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,11 @@ rootProject.name = "vs-core-kt"
 pluginManagement {
     includeBuild("build-logic")
 }
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("coreLibs") {
+            from(files("core-libs.versions.toml"))
+        }
+    }
+}
