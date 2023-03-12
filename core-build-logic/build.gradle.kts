303ae90273ff350785bf4f8407b99c262df3c18c
Vladislav Sumin <vladislavsumin@mail.ru>
2023-03-12 12:43:05 +0300

    add test publish pipeline

--- core-build-logic/build.gradle.kts
+++ core-build-logic/build.gradle.kts
@@ -1,5 +1,22 @@
 plugins {
     `kotlin-dsl`
+    `maven-publish`
+}
+
+group = "ru.vs.core"
+version = "0.0.2"
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/vladislavsumin/vs-core-kt")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password =System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 dependencies {
