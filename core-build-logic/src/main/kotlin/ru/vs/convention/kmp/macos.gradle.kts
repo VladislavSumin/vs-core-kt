6430ea3a76808aaf0cddf7df9e0d92cddfb3b735
Vladislav Sumin <vladislavsumin@mail.ru>
2023-04-24 21:29:37 +0300

    setup macos compose target

--- core-build-logic/src/main/kotlin/ru/vs/convention/kmp/macos.gradle.kts
+++ core-build-logic/src/main/kotlin/ru/vs/convention/kmp/macos.gradle.kts
@@ -7,4 +7,20 @@ plugins {
 kotlin {
     macosX64()
     macosArm64()
+
+    sourceSets {
+        val commonMain by getting {}
+
+        val macosMain by creating {
+            dependsOn(commonMain)
+        }
+
+        val macosX64Main by getting {
+            dependsOn(macosMain)
+        }
+
+        val macosArm64Main by getting {
+            dependsOn(macosMain)
+        }
+    }
 }
