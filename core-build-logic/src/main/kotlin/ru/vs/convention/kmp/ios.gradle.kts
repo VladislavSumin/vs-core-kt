a61cb60a677d6efeb0d4513c5d928fedb108596c
Vladislav Sumin <vladislavsumin@mail.ru>
2023-04-24 22:04:53 +0300

    setup ios target

--- core-build-logic/src/main/kotlin/ru/vs/convention/kmp/ios.gradle.kts
+++ core-build-logic/src/main/kotlin/ru/vs/convention/kmp/ios.gradle.kts
@@ -5,7 +5,24 @@ plugins {
 }
 
 kotlin {
-    ios()
-    iosSimulatorArm64()
-    iosArm64()
+    //TODO мапинг названий нужен что бы композ догадался где что искарть, ищет он по таким именам а поменять нельзя
+    iosX64("uikitX64")
+    iosArm64("uikitArm64")
+    // iosSimulatorArm64("uikitSimulatorArm64")
+
+    sourceSets {
+        val commonMain by getting {}
+
+        val uikitMain by creating {
+            dependsOn(commonMain)
+        }
+
+        val uikitX64Main by getting {
+            dependsOn(uikitMain)
+        }
+
+        val uikitArm64Main by getting {
+            dependsOn(uikitMain)
+        }
+    }
 }
