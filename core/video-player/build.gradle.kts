570885d6a87ca5c395b5cfa1d808f71a17bd7d1b
Vladislav Sumin <vladislavsumin@mail.ru>
2023-05-14 09:20:02 +0300

    copy video player from jb samples

---
+++ core/video-player/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+
+    id("ru.vs.convention.kmp.android-library")
+    id("ru.vs.convention.kmp.jvm")
+    id("ru.vs.convention.kmp.ios")
+    id("ru.vs.convention.kmp.js")
+    // id("ru.vs.convention.kmp.windows") compose (for desktop) don't support windows now
+    // id("ru.vs.convention.kmp.linux") compose (for desktop) don't support linux now
+    id("ru.vs.convention.kmp.macos")
+
+    // KMM Test library don't support wasm now
+    // id("ru.vs.convention.kmp.wasm")
+
+    id("org.jetbrains.compose")
+}
+
+android {
+    namespace = "ru.vs.core.video_player"
+}
+
+kotlin {
+    sourceSets {
+        named("commonMain") {
+            dependencies {
+                implementation(projects.core.compose)
+            }
+        }
+
+        named("jvmMain"){
+            dependencies{
+                implementation(coreLibs.vlcj)
+            }
+        }
+    }
+}
