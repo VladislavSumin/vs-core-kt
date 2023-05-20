5bd22eb2a8dd700cc0d35f47f75fe96c3115afe8
Vladislav Sumin <vladislavsumin@mail.ru>
2023-05-20 23:24:05 +0300

    add jraska assertion plugin

---
+++ core-build-logic/src/main/kotlin/ru/vs/convention/analyze/check-module-graph.gradle.kts
@@ -0,0 +1,15 @@
+package ru.vs.convention.analyze
+
+/**
+ * Convention for default setup Module Graph Assert
+ * See https://github.com/jraska/modules-graph-assert
+ */
+
+plugins {
+    id("com.jraska.module.graph.assertion")
+}
+
+moduleGraphAssert {
+    // TODO request feature to define configurations as regexp
+    configurations += setOf("commonMainImplementation", "commonMainApi")
+}
