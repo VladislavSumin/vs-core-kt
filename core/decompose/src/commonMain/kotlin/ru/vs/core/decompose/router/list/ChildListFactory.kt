9557bdc3f58ac0120a0fd14f46df965ad03295b2
Vladislav Sumin <vladislavsumin@mail.ru>
2023-05-22 23:58:36 +0300

    simplify ChildListFactory.kt

--- core/decompose/src/commonMain/kotlin/ru/vs/core/decompose/router/list/ChildListFactory.kt
+++ core/decompose/src/commonMain/kotlin/ru/vs/core/decompose/router/list/ChildListFactory.kt
@@ -6,36 +6,31 @@ import com.arkivanov.decompose.router.children.*
 import com.arkivanov.decompose.value.Value
 
 fun <C : Any, T : Any> ComponentContext.childList(
-        source: NavigationSource<List<C>>,
-        initialStack: () -> List<C> = { emptyList() },
-        key: String = "DefaultChildList",
-        childFactory: (configuration: C, ComponentContext) -> T,
-): Value<ChildList<C, T>> =
-        children(
-                source = source,
-                key = key,
-                initialState = { ListNavState(configurations = initialStack()) },
-                saveState = { null },
-                restoreState = { ListNavState(initialStack()) },
-                navTransformer = { _, event -> ListNavState(configurations = event) },
-                stateMapper = { _, children ->
-                    @Suppress("UNCHECKED_CAST")
-                    val createdChildren = children as List<Child.Created<C, T>>
-
-                    ChildList(items = createdChildren)
-                },
-                onEventComplete = { _, _, _ -> /* no action */ },
-                backTransformer = { _ -> null },
-                childFactory = childFactory,
-        )
+    source: NavigationSource<List<C>>,
+    initialStack: () -> List<C> = { emptyList() },
+    key: String = "DefaultChildList",
+    childFactory: (configuration: C, ComponentContext) -> T,
+): Value<List<Child.Created<C, T>>> = children(
+    source = source,
+    key = key,
+    initialState = { ListNavState(configurations = initialStack()) },
+    saveState = { null },
+    restoreState = { ListNavState(initialStack()) },
+    navTransformer = { _, event -> ListNavState(configurations = event) },
+    stateMapper = { _, children ->
+        @Suppress("UNCHECKED_CAST")
+        children as List<Child.Created<C, T>>
+    },
+    onEventComplete = { _, _, _ -> /* no action */ },
+    backTransformer = { _ -> null },
+    childFactory = childFactory,
+)
 
 private data class ListNavState<out C : Any>(val configurations: List<C>) : NavState<C> {
     override val children: List<SimpleChildNavState<C>> = configurations.map { configuration ->
         SimpleChildNavState(
-                configuration = configuration,
-                status = ChildNavState.Status.ACTIVE,
+            configuration = configuration,
+            status = ChildNavState.Status.ACTIVE,
         )
     }
 }
-
-data class ChildList<out C : Any, out T : Any>(val items: List<Child.Created<C, T>>)
