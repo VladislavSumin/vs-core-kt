9b8dfae75028b14ba2fb787c52abe23cb27fb2ba
Vladislav Sumin <vladislavsumin@mail.ru>
2023-05-26 15:06:52 +0300

    update childListWithState to remove abuse configuration logic, and avoid hash map calculation on each update

--- core/decompose/src/commonMain/kotlin/ru/vs/core/decompose/router/list/ChildListWithStateFactory.kt
+++ core/decompose/src/commonMain/kotlin/ru/vs/core/decompose/router/list/ChildListWithStateFactory.kt
@@ -5,6 +5,7 @@ import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.children.*
 import com.arkivanov.decompose.value.Value
 import com.arkivanov.decompose.value.operator.map
+import com.arkivanov.essenty.lifecycle.doOnDestroy
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -19,7 +20,6 @@ import kotlinx.coroutines.flow.StateFlow
  * @param idSelector - get unique id for any source list element
  *
  * TODO write performance tests to it
- * TODO try LinkedHashMap as [source]
  */
 fun <C : Any, ID : Any, T : ComponentContext> ComponentContext.childListWithState(
     source: NavigationSource<List<C>>,
@@ -27,77 +27,59 @@ fun <C : Any, ID : Any, T : ComponentContext> ComponentContext.childListWithStat
     initialStack: () -> List<C> = { emptyList() },
     key: String = "DefaultChildList",
     childFactory: (state: StateFlow<C>, ComponentContext) -> T,
-): Value<List<T>> = children(
-    source = source,
-    key = key,
-    initialState = { ListWithStateNavState(initialStack(), idSelector) },
-    saveState = { null },
-    restoreState = { ListWithStateNavState(initialStack(), idSelector) },
-    navTransformer = { oldState, event ->
-        val oldStateMap = oldState.configurations.associateBy { it.id }
-        val newState = event.map { item ->
-            val id = idSelector(item)
-            val state = oldStateMap[id]
-            if (state != null) {
-                // if we found old item with same id
-                // then just update it internal state
-                state.state.value = item
-                state
-            } else {
-                // otherwise it's new item
-                // create configuration for it
-                ListWithStateConfiguration(id, item)
+): Value<List<T>> {
+    val cache = mutableMapOf<ID, MutableStateFlow<C>>()
+
+    fun updateCache(configurations: List<C>) {
+        configurations.forEach { configuration ->
+            cache.compute(idSelector(configuration)) { _, state ->
+                state?.also { it.value = configuration } ?: MutableStateFlow(configuration)
             }
-
-            // we don't need to delete old items, because we mapped new configuration list
-            // decompose internals cleanup deleted items lifecycle automatically
         }
-        ListWithStateNavState(configurations = newState)
-    },
-    stateMapper = { _, children ->
-        @Suppress("UNCHECKED_CAST")
-        children as List<Child.Created<C, T>>
-    },
-    onEventComplete = { _, _, _ -> /* no action */ },
-    backTransformer = { _ -> null },
-    childFactory = { state, context -> childFactory(state.state, context) },
-).map { childList -> childList.map { it.instance } }
-
-/**
- * Data class with override equals and hashcode to check only [id] field and skip [state] field
- */
-private data class ListWithStateConfiguration<C : Any, ID : Any>(
-    val id: ID,
-    val state: MutableStateFlow<C>,
-) {
-    constructor(id: ID, state: C) : this(id, MutableStateFlow(state))
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as ListWithStateConfiguration<*, *>
-
-        return id == other.id
     }
 
-    override fun hashCode(): Int {
-        return id.hashCode()
+    fun createInitialState(): ListWithStateNavState<ID> {
+        val stack = initialStack()
+        updateCache(stack)
+        return ListWithStateNavState(stack.map(idSelector))
     }
+
+    return children(
+        source = source,
+        key = key,
+        initialState = { createInitialState() },
+        saveState = { null },
+        restoreState = { createInitialState() },
+        navTransformer = { _, event ->
+            updateCache(event)
+            ListWithStateNavState(configurations = event.map(idSelector))
+        },
+        stateMapper = { _, children ->
+            @Suppress("UNCHECKED_CAST")
+            children as List<Child.Created<C, T>>
+        },
+        onEventComplete = { _, _, _ -> /* no action */ },
+        backTransformer = { _ -> null },
+        childFactory = { id, context ->
+            val stateFlow = cache[id]!!
+            childFactory(stateFlow, context).apply {
+                lifecycle.doOnDestroy {
+                    cache.remove(id)
+                }
+            }
+        },
+    ).map { childList -> childList.map { it.instance } }
 }
 
-private data class ListWithStateNavState<C : Any, ID : Any>(
-    val configurations: List<ListWithStateConfiguration<C, ID>>
-) : NavState<ListWithStateConfiguration<C, ID>> {
-    override val children: List<SimpleChildNavState<ListWithStateConfiguration<C, ID>>> =
+
+private data class ListWithStateNavState<ID : Any>(
+    val configurations: List<ID>
+) : NavState<ID> {
+    override val children: List<ChildNavState<ID>> =
         configurations.map { configuration ->
             SimpleChildNavState(
                 configuration = configuration,
                 status = ChildNavState.Status.ACTIVE,
             )
         }
-
-    constructor(configurations: List<C>, idSelector: (C) -> ID) : this(
-        configurations.map { ListWithStateConfiguration(idSelector(it), it) }
-    )
 }
