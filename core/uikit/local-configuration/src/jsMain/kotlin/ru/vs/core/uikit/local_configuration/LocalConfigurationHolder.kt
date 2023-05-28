3c63612fe7125c2e1aa6645e8b5979bbaad9be78
Vladislav Sumin <vladislavsumin@mail.ru>
2023-05-28 10:39:25 +0300

    add stub configuration holder for js

--- core/uikit/local-configuration/src/jsMain/kotlin/ru/vs/core/uikit/local_configuration/LocalConfigurationHolder.kt
+++ core/uikit/local-configuration/src/jsMain/kotlin/ru/vs/core/uikit/local_configuration/LocalConfigurationHolder.kt
@@ -3,8 +3,16 @@ package ru.vs.core.uikit.local_configuration
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
 
 @Composable
 actual fun LocalConfigurationHolder(content: @Composable () -> Unit) {
-    TODO("Not implemented")
+
+    val configuration = remember { ConfigurationImpl() }
+
+    // TODO now just hardcode for test js run, need add correct listener
+    configuration.screenWidth = 1200.dp
+    configuration.screenHeight = 600.dp
+
+    CompositionLocalProvider(LocalConfiguration provides configuration, content = content)
 }
