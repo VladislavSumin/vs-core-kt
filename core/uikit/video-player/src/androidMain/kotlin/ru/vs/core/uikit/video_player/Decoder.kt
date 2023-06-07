1d3689e6f467783970b8a994200880a204ce16e1
Vladislav Sumin <vladislavsumin@mail.ru>
2023-06-07 20:55:58 +0300

    some decoder tests

--- core/uikit/video-player/src/androidMain/kotlin/ru/vs/core/uikit/video_player/Decoder.kt
+++ core/uikit/video-player/src/androidMain/kotlin/ru/vs/core/uikit/video_player/Decoder.kt
@@ -8,6 +8,7 @@ import io.github.oshai.KotlinLogging
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
+import java.nio.ByteBuffer
 
 
 internal class Decoder(
@@ -16,24 +17,68 @@ internal class Decoder(
 ) {
 
     fun init(surface: Surface) {
-        val codec = MediaCodec.createByCodecName("video/avc")
+        val codec = MediaCodec.createDecoderByType("video/avc")
         val format = MediaFormat()
         format.setString(MediaFormat.KEY_MIME, "video/avc")
-        format.setInteger(MediaFormat.KEY_WIDTH, 800)
-        format.setInteger(MediaFormat.KEY_HEIGHT, 600)
+        format.setInteger(MediaFormat.KEY_WIDTH, 2920)
+        format.setInteger(MediaFormat.KEY_HEIGHT, 2080)
         codec.configure(format, surface, null, 0)
+        codec.start()
 
-//        scope.launch(newSingleThreadContext("writter")) {
-//            source.data.collect { inputByteArray ->
+        scope.launch(newSingleThreadContext("writter")) {
+            var index = -1
+            var buffer: ByteBuffer? = null
+            var time = 0L
+
+            fun takeBuffer() {
+                while (index < 0) {
+                    index = codec.dequeueInputBuffer(30000)
+                }
+                buffer = codec.getInputBuffer(index)!!
+            }
+
+            fun releaseBuffer() {
+                codec.queueInputBuffer(index, 0, buffer!!.position(), time, 0)
+                time += 1
+                buffer = null
+                index = -1
+            }
+
+            fun updateBuffer() {
+                releaseBuffer()
+                takeBuffer()
+            }
+
+
+            takeBuffer()
+            source.data.collect { inputByteArray ->
+
+                if (buffer!!.remaining() < inputByteArray.size || buffer!!.position()>200_000) updateBuffer()
+                buffer!!.put(inputByteArray)
+
 //                var index = -1
 //                while (index < 0) {
 //                    index = codec.dequeueInputBuffer(30000)
 //                }
+//                logger.info { "put()" }
 //                val buffer = codec.getInputBuffer(index)!!
-//                buffer.
-//            }
-//        }
-//        scope.launch(newSingleThreadContext("reader")) { }
+//                check(buffer.limit() >= inputByteArray.size)
+//                check(buffer.position() == 0)
+//                buffer.put(inputByteArray)
+//                codec.queueInputBuffer(index, 0, inputByteArray.size, 0, 0)
+            }
+        }
+        scope.launch(newSingleThreadContext("reader")) {
+            while (true) {
+                val newBufferInfo = MediaCodec.BufferInfo()
+                var index = -1
+                while (index < 0) {
+                    index = codec.dequeueOutputBuffer(newBufferInfo, 30000)
+                }
+                logger.info { "get()" }
+                codec.releaseOutputBuffer(index, true)
+            }
+        }
 
     }
 
