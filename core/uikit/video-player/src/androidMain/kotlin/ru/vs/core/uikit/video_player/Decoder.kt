c9a5ab5373fab5ea3f61d898ff23fd9b1df0e7ed
Vladislav Sumin <vladislavsumin@mail.ru>
2023-06-10 16:37:10 +0300

    refactoring decoder

--- core/uikit/video-player/src/androidMain/kotlin/ru/vs/core/uikit/video_player/Decoder.kt
+++ core/uikit/video-player/src/androidMain/kotlin/ru/vs/core/uikit/video_player/Decoder.kt
@@ -1,165 +1,175 @@
 package ru.vs.core.uikit.video_player
 
 import android.media.MediaCodec
-import android.media.MediaCodecInfo
-import android.media.MediaCodecList
 import android.media.MediaFormat
-import android.view.Surface
-import io.github.oshai.KotlinLogging
+import android.view.SurfaceHolder
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
 import java.nio.ByteBuffer
 
 
 internal class Decoder(
     private val source: Playable.FlowOfByteArrays,
-    private val scope: CoroutineScope,
-) {
+) : SurfaceHolder.Callback {
+    private var scope: CoroutineScope? = null
 
-    private fun selectCodec(mimeType: String) {
-        val numCodecs = MediaCodecList.getCodecCount()
-        for (i in 0 until numCodecs) {
-            val codecInfo = MediaCodecList.getCodecInfoAt(i)
-            if (!codecInfo.supportedTypes.contains("video/avc")) continue
-            if (codecInfo.isEncoder) continue
-            println("====================")
-            println(codecInfo.name)
-            println(codecInfo.canonicalName)
-            println("====================")
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        check(scope == null)
+        scope = CoroutineScope(Dispatchers.IO)
+
+        scope!!.launch {
+
+            // Don't use create by media type because default codec can't parse given video stream
+            val mediaCodec = MediaCodec.createByCodecName("c2.android.avc.decoder")
+            mediaCodec.configure(createMediaFormat(), holder.surface, null, 0)
+            mediaCodec.start()
+
+            try {
+                launch { parseAndSubmitFramesToCodec(mediaCodec) }
+                renderDecodedData(mediaCodec)
+            } finally {
+                mediaCodec.stop()
+            }
         }
     }
 
-    fun init(surface: Surface) {
-        selectCodec("video/avc")
-        logger.info { "init()" }
-        val codec = MediaCodec.createByCodecName("c2.android.avc.decoder")
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        // no action
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        scope!!.cancel()
+        scope = null
+    }
+
+    /**
+     * Read [MediaCodec.dequeueOutputBuffer] and render its to given surface
+     */
+    private suspend fun renderDecodedData(mediaCodec: MediaCodec) {
+        val newBufferInfo = MediaCodec.BufferInfo()
+        while (true) {
+            var index = -1
+            while (index < 0) {
+                index = mediaCodec.dequeueOutputBuffer(newBufferInfo, 30000)
+            }
+            mediaCodec.releaseOutputBuffer(index, true)
+
+            // TODO pass correct frame rate to here
+            delay(8)
+        }
+    }
+
+    private suspend fun parseAndSubmitFramesToCodec(mediaCodec: MediaCodec) {
+        var bufferIndex = -1
+        var buffer: ByteBuffer? = null
+
+        fun takeBuffer() {
+            while (bufferIndex < 0) {
+                bufferIndex = mediaCodec.dequeueInputBuffer(30000)
+            }
+            buffer = mediaCodec.getInputBuffer(bufferIndex)!!
+            check(buffer!!.position() == 0)
+            buffer!!.put(0)
+            buffer!!.put(0)
+            buffer!!.put(1)
+        }
+
+        fun releaseBuffer() {
+            mediaCodec.queueInputBuffer(bufferIndex, 0, buffer!!.position(), 0, 0)
+            buffer = null
+            bufferIndex = -1
+        }
+
+        fun updateBuffer() {
+            releaseBuffer()
+            takeBuffer()
+        }
+
+        var isNaluFind = false
+        var isFirstNaluFind = false
+        var zerosCount = 0
+        var emitterIndex = 0
+
+        fun findFirstNalu(bArr: ByteArray) {
+            while (true) {
+                val length = bArr.size
+                val i: Int = emitterIndex
+                if (length > i) {
+                    val b = bArr[i]
+                    if (b.toInt() == 0) {
+                        zerosCount++
+                    } else if (b.toInt() != 1 || zerosCount < 2) {
+                        zerosCount = 0
+                    } else {
+                        zerosCount = 0
+                        emitterIndex = i + 1
+                        isFirstNaluFind = true
+                        return
+                    }
+                    emitterIndex = i + 1
+                } else {
+                    return
+                }
+            }
+        }
+
+        fun processInput(bArr: ByteArray) {
+            while (true) {
+                val length = bArr.size
+                val i: Int = emitterIndex
+                if (length > i) {
+                    val b = bArr[i]
+                    buffer!!.put(b)
+                    if (isNaluFind) {
+                        if (zerosCount > 3) zerosCount = 3
+                        buffer!!.position(buffer!!.position() - zerosCount - 2)
+                        updateBuffer()
+
+                        isNaluFind = false
+                        zerosCount = 0
+                        buffer!!.put(b)
+                    } else if (b.toInt() == 0) {
+                        zerosCount++
+                    } else if (b.toInt() != 1 || zerosCount < 2) {
+                        zerosCount = 0
+                    } else {
+                        isNaluFind = true
+                    }
+                    emitterIndex++
+                } else {
+                    return
+                }
+            }
+        }
+
+        fun processNextInput(bArr: ByteArray) {
+            emitterIndex = 0
+            if (!isFirstNaluFind) {
+                findFirstNalu(bArr)
+            }
+            processInput(bArr)
+        }
+
+        takeBuffer()
+        source.data.collect { inputByteArray ->
+            processNextInput(inputByteArray)
+        }
+    }
+
+    /**
+     * Creates [MediaFormat] with default config
+     */
+    private fun createMediaFormat(): MediaFormat {
         val format = MediaFormat()
         format.setString(MediaFormat.KEY_MIME, "video/avc")
+
+        // It's ok to pass any resolution here
+        // Resolution set to correct automatically when codec receive frame with configuration
         format.setInteger(MediaFormat.KEY_WIDTH, 800)
         format.setInteger(MediaFormat.KEY_HEIGHT, 600)
-        codec.configure(format, surface, null, 0)
-        codec.start()
-
-        scope.launch(newSingleThreadContext("writter")) {
-            var bufferIndex = -1
-            var buffer: ByteBuffer? = null
-            var time = 0L
-
-            fun takeBuffer(zerosCount: Int) {
-                while (bufferIndex < 0) {
-                    bufferIndex = codec.dequeueInputBuffer(30000)
-                }
-                buffer = codec.getInputBuffer(bufferIndex)!!
-                check(buffer!!.position() == 0)
-                repeat(zerosCount) {
-                    buffer!!.put(0)
-                }
-                buffer!!.put(1)
-            }
-
-            fun releaseBuffer() {
-                logger.info { "put() pos = ${buffer!!.position()}" }
-                codec.queueInputBuffer(bufferIndex, 0, buffer!!.position(), time, 0)
-                time += 1000000
-                buffer = null
-                bufferIndex = -1
-            }
-
-            fun updateBuffer(zerosCount: Int) {
-                releaseBuffer()
-                takeBuffer(zerosCount)
-            }
-
-            var isNaluFind = false
-            var isFirstNaluFind = false
-            var zerosCount = 0
-            var emitterIndex = 0
-
-            fun findFirstNalu(bArr: ByteArray) {
-                while (true) {
-                    val length = bArr.size
-                    val i: Int = emitterIndex
-                    if (length > i) {
-                        val b = bArr[i]
-                        if (b.toInt() == 0) {
-                            zerosCount++
-                        } else if (b.toInt() != 1 || zerosCount < 2) {
-                            zerosCount = 0
-                        } else {
-                            zerosCount = 0
-                            emitterIndex = i + 1
-                            isFirstNaluFind = true
-                            return
-                        }
-                        emitterIndex = i + 1
-                    } else {
-                        return
-                    }
-                }
-            }
-
-            fun processInput(bArr: ByteArray) {
-                while (true) {
-                    val length = bArr.size
-                    val i: Int = emitterIndex
-                    if (length > i) {
-                        val b = bArr[i]
-                        buffer!!.put(b)
-                        if (isNaluFind) {
-                            logger.info { "zeros count = $zerosCount" }
-                            if (zerosCount > 2) zerosCount = 2
-                            buffer!!.position(buffer!!.position() - zerosCount - 2)
-                            updateBuffer(zerosCount)
-                            logger.info { "naluType = $b, v2= ${(b.toInt() and 0b1111111).toByte()}" }
-
-                            isNaluFind = false
-                            zerosCount = 0
-                            buffer!!.put(b)
-                        } else if (b.toInt() == 0) {
-                            zerosCount++
-                        } else if (b.toInt() != 1 || zerosCount < 2) {
-                            zerosCount = 0
-                        } else {
-                            isNaluFind = true
-                        }
-                        emitterIndex++
-                    } else {
-                        return
-                    }
-                }
-            }
-
-            fun processNextInput(bArr: ByteArray) {
-                emitterIndex = 0
-                if (!isFirstNaluFind) {
-                    findFirstNalu(bArr)
-                }
-                processInput(bArr)
-            }
-
-            takeBuffer(zerosCount)
-            source.data.collect { inputByteArray ->
-                processNextInput(inputByteArray)
-            }
-        }
-        scope.launch(newSingleThreadContext("reader")) {
-            val newBufferInfo = MediaCodec.BufferInfo()
-            while (true) {
-                var index = -1
-                while (index < 0) {
-                    index = codec.dequeueOutputBuffer(newBufferInfo, 30000)
-                }
-                logger.info { "get()" }
-                codec.releaseOutputBuffer(index, true)
-                delay(8)
-            }
-        }
-
-    }
-
-    companion object {
-        private val logger = KotlinLogging.logger("MediaDecoder")
+        return format
     }
 }
--- core/uikit/video-player/src/androidMain/kotlin/ru/vs/core/uikit/video_player/VideoPlayerImpl.kt
+++ core/uikit/video-player/src/androidMain/kotlin/ru/vs/core/uikit/video_player/VideoPlayerImpl.kt
@@ -1,10 +1,8 @@
 package ru.vs.core.uikit.video_player
 
-import android.view.SurfaceHolder
 import android.view.SurfaceView
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import io.github.oshai.KotlinLogging
@@ -16,38 +14,19 @@ internal actual fun VideoPlayerImpl(
     source: Playable,
     modifier: Modifier,
 ) {
-    val scope = rememberCoroutineScope()
-
-    val decoder = remember(source, scope) {
-        Decoder(source as Playable.FlowOfByteArrays, scope)
-    }
-
-    val surfaceCallback = remember {
-        object : SurfaceHolder.Callback {
-            override fun surfaceCreated(holder: SurfaceHolder) {
-                logger.info { "surfaceCreated()" }
-                decoder.init(holder.surface)
-            }
-
-            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-                logger.info { "surfaceChanged(format=$format, width=$width, height=$height)" }
-            }
-
-            override fun surfaceDestroyed(holder: SurfaceHolder) {
-                logger.info { "surfaceDestroyed()" }
-            }
-        }
+    val decoder = remember(source) {
+        Decoder(source as Playable.FlowOfByteArrays)
     }
 
     AndroidView(
         factory = { context ->
             logger.info { "AndroidView.factory()" }
             SurfaceView(context).also { surfaceView ->
-                surfaceView.holder.addCallback(surfaceCallback)
+                surfaceView.holder.addCallback(decoder)
             }
         },
         modifier
     ) {
-
+        // no action
     }
 }
