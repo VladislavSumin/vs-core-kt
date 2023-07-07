137b3b4397054f35737b7d58be151934087a8928
Vladislav Sumin <vladislavsumin@mail.ru>
2023-07-07 23:14:41 +0300

    split common && common-test conventions

--- core-build-logic/src/main/kotlin/ru/vs/convention/kmp/common.gradle.kts
+++ core-build-logic/src/main/kotlin/ru/vs/convention/kmp/common.gradle.kts
@@ -1,24 +1,6 @@
 package ru.vs.convention.kmp
 
-import org.gradle.api.tasks.testing.Test
-import org.gradle.kotlin.dsl.named
-import org.gradle.kotlin.dsl.withType
-
 plugins {
     id("kotlin-multiplatform")
-}
-
-kotlin {
-    sourceSets {
-        named("commonTest") {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
+    id("ru.vs.convention.kmp.common-tests")
 }
