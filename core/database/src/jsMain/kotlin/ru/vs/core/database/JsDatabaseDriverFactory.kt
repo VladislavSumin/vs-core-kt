e83f4105be72a089caf4e0852cce971dd795dad0
Vladislav Sumin <vladislavsumin@mail.ru>
2023-11-30 00:54:30 +0300

    fix js target assembling

--- core/database/src/jsMain/kotlin/ru/vs/core/database/JsDatabaseDriverFactory.kt
+++ core/database/src/jsMain/kotlin/ru/vs/core/database/JsDatabaseDriverFactory.kt
@@ -1,14 +1,15 @@
 package ru.vs.core.database
 
-import app.cash.sqldelight.db.SqlDriver
+//import app.cash.sqldelight.driver.sqljs.initSqlDriver
+import app.cash.sqldelight.db.QueryResult
 import app.cash.sqldelight.db.SqlSchema
-import app.cash.sqldelight.driver.sqljs.initSqlDriver
-import kotlinx.coroutines.await
+import app.cash.sqldelight.db.SqlDriver
 import org.kodein.di.DirectDI
 
 private class JsDatabaseDriverFactory : DatabaseDriverFactory {
-    override suspend fun create(schema: SqlSchema): SqlDriver {
-        return initSqlDriver(schema).await()
+    override suspend fun create(name: String, schema: SqlSchema<QueryResult.Value<Unit>>): SqlDriver {
+        // return initSqlDriver(schema).await()
+        TODO()
     }
 }
 
