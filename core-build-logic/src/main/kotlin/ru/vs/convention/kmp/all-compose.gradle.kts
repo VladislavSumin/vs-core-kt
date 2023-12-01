8619f689b8a006e8620f26b09db677298d1d664d
Vladislav Sumin <vladislavsumin@mail.ru>
2023-12-01 21:47:13 +0300

    add all-compose convention plugin

---
+++ core-build-logic/src/main/kotlin/ru/vs/convention/kmp/all-compose.gradle.kts
@@ -0,0 +1,18 @@
+package ru.vs.convention.kmp
+
+/**
+ * Apply all platform supported by jb compose
+ */
+
+plugins {
+    id("ru.vs.convention.kmp.android-library")
+    id("ru.vs.convention.kmp.jvm")
+    id("ru.vs.convention.kmp.ios")
+    id("ru.vs.convention.kmp.js")
+    // id("ru.vs.convention.kmp.windows") compose (for desktop) don't support windows now
+    // id("ru.vs.convention.kmp.linux") compose (for desktop) don't support linux now
+    id("ru.vs.convention.kmp.macos")
+
+    // KMM Test library don't support wasm now
+    // id("ru.vs.convention.kmp.wasm")
+}
--- core/compose/build.gradle.kts
+++ core/compose/build.gradle.kts
@@ -1,18 +1,7 @@
 import ru.vs.build_logic.coreConfiguration
 
 plugins {
-
-    id("ru.vs.convention.kmp.android-library")
-    id("ru.vs.convention.kmp.jvm")
-    id("ru.vs.convention.kmp.ios")
-    id("ru.vs.convention.kmp.js")
-    // id("ru.vs.convention.kmp.windows") compose (for desktop) don't support windows now
-    // id("ru.vs.convention.kmp.linux") compose (for desktop) don't support linux now
-    id("ru.vs.convention.kmp.macos")
-
-    // KMM Test library don't support wasm now
-    // id("ru.vs.convention.kmp.wasm")
-
+    id("ru.vs.convention.kmp.all-compose")
     id("ru.vs.convention.compose")
 }
 
